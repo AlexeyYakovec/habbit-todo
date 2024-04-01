f50a3e685b94d8eace23134aed16fcff1289f3e3
AlexeyYakovec <yakovec8952@gmail.com>
2024-04-01 18:24:27 +0500

    upgrade menu-list

--- scripts/app.js
+++ scripts/app.js
@@ -3,14 +3,19 @@
 let habbits = [];
 const HABBIT_KEY = "HABBIT_KEY";
 
+/* components */
+const page = {
+  menu: document.querySelector(".menu__list"),
+};
+
 // utils
 
 // функция загрузки данных
 function loadData() {
   const habbitsString = localStorage.getItem(HABBIT_KEY);
-  const habbitArray = JSON.parse(habbitsString);
-  if (Array.isArray(habbitArray)) {
-    habbits = habbitArray;
+  const habbitsArray = JSON.parse(habbitsString);
+  if (Array.isArray(habbitsArray)) {
+    habbits = habbitsArray;
   }
 }
 
@@ -21,14 +26,43 @@ function saveData() {
 
 // render
 
-function renderMenu(activeHabbit) {
+function rerenderMenu(activeHabbit) {
   if (!activeHabbit) {
     return;
   }
   for (const habbit of habbits) {
-    existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+    const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
     if (!existed) {
-      // создание
+      const element = document.createElement("li");
+      element.classList.add("menu__list-item");
+      element.setAttribute("menu-habbit-id", habbit.id);
+      const elementButton = document.createElement("button");
+      elementButton.classList.add("menu__button", "button");
+      elementButton.innerHTML = `<img src="./images/${habbit.icon}.svg" alt="${habbit.name}" />`;
+      element.appendChild(elementButton);
+      if (activeHabbit.id === habbit.ad) {
+        elementButton.classList.add("menu__button-active");
+      }
+      page.menu.appendChild(element);
+      continue;
+    }
+    if (activeHabbit.id === habbit.id) {
+      existed.classList.add("menu__button-active");
+    } else {
+      existed.classList.remove("menu__button-active");
     }
   }
 }
+
+function rerender(activeHabbitId) {
+  const activeHabbit = habbits.find((habbit) => {
+    return habbit.id === activeHabbitId;
+  });
+  rerenderMenu(activeHabbit);
+}
+
+// init
+(() => {
+  loadData();
+  rerender(habbits[0].id);
+})();
