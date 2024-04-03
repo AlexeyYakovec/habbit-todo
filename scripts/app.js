07df90f813a4c4e9dbd5819cefdab003f5079781
AlexeyYakovec <yakovec8952@gmail.com>
2024-04-03 14:48:43 +0500

    application completed with localStorage

--- scripts/app.js
+++ scripts/app.js
@@ -241,6 +241,7 @@ function rerender(activeHabbitId) {
    if (!activeHabbit) {
       return;
    }
+   document.location.replace(document.location.pathname + "#" + activeHabbitId);
    rerenderMenu(activeHabbit);
    rerenderHead(activeHabbit);
    rerenderContent(activeHabbit);
@@ -249,6 +250,13 @@ function rerender(activeHabbitId) {
 // init
 (() => {
    loadData();
-   rerender(habbits[0].id);
+   const hashId = Number(document.location.hash.replace("#", ""));
+   console.log(hashId);
+   const urlHabbit = habbits.find((habbit) => habbit.id == hashId);
+   if (urlHabbit) {
+      rerender(urlHabbit.id);
+   } else {
+      rerender(habbits[0].id);
+   }
    togglePopup();
 })();
